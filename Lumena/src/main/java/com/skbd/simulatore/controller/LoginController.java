61a23854237cab4b28749e87496dc2b3f4f12786
Scarpe di gomma <marco.rigotti@buonarroti.tn.it>
2026-04-10 11:40:54 +0200

    (re)added views, css and controllers for the main pages

---
+++ Lumena/src/main/java/com/skbd/simulatore/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.skbd.simulatore.controller;
+
+import javafx.event.ActionEvent;
+
+public class LoginController {
+    public void handleLogin(ActionEvent actionEvent) {
+    }
+
+    public void handleGoToRegister(ActionEvent actionEvent) {
+    }
+}
--- Lumena/src/main/java/module-info.java
+++ Lumena/src/main/java/module-info.java
@@ -1,16 +1,19 @@
 module com.skbd.simulatore {
+
     requires javafx.controls;
     requires javafx.fxml;
 
-    //per sql
-    requires java.sql;
+
 
     //per smile, il modello predittivo
     requires smile.core;
+
     //requires smile.math;
     //requires smile.data;
 
     opens com.skbd.simulatore.view to javafx.fxml;
+    opens com.skbd.simulatore.controller to javafx.fxml;
 
     exports com.skbd.simulatore.view;
+    exports com.skbd.simulatore.controller;
 }
